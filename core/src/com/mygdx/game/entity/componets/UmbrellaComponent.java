7e65c1276b53bed6771e951037fe9a00c4f48822
Teatree <4aynoedepebo@gmail.com>
2015-12-29 22:56:41 -0800

    * Finished Bezier movement for umbrella

--- core/src/com/mygdx/game/entity/componets/UmbrellaComponent.java
+++ core/src/com/mygdx/game/entity/componets/UmbrellaComponent.java
@@ -11,43 +11,22 @@ import java.util.Random;
  * Created by Teatree on 10/6/2015.
  */
 public class UmbrellaComponent implements Component {
-    public float speedIncrCoeficient = 1f;
-    public float gravityDecreaseMultiplier = 1.5f;
 
     public Rectangle boundsRect = new Rectangle();
-    public float velocityX;
-    public float velocityY;
-    public float gravity;
+
+    public Bezier<Vector2> myCatmull;
+    public float speed = 0.15f;
+    public float current = 0;
+    public Vector2 out;
+    public Vector2[] dataSet;
 
     public State state;
     public int pointsMult;
-
-    public CatmullRomSpline<Vector2> myCatmull;
-    public float speed = 0.25f;
-    public float current = 0;
-    public Vector2 out;
-
-    public float startX, startY;
-    public float endX = Gdx.graphics.getWidth()-120;
-    public float endY = Gdx.graphics.getHeight()/2;
-    public float duration = 17;
-    public float time;
-    public Interpolation interpolation = Interpolation.bounce;
-    public boolean reverse, began, complete;
-    public int alignment = Align.bottomLeft;
 
     public UmbrellaComponent() {
         this.state = UmbrellaComponent.State.PUSH;
         pointsMult = 2;
 
-        Vector2[] dataSet = new Vector2[2];
-        dataSet[0] = new Vector2(Gdx.graphics.getWidth()-100, Gdx.graphics.getHeight()/2);
-        dataSet[1] = new Vector2(40, Gdx.graphics.getHeight()/2);
-
-        myCatmull = new CatmullRomSpline<Vector2>(dataSet, true);
-        out = new Vector2(340, Gdx.graphics.getHeight()/4);
-        myCatmull.valueAt(out, 5);
-        myCatmull.derivativeAt(out, 5);
     }
 
     public enum State {
--- core/src/com/mygdx/game/system/UmbrellaSystem.java
+++ core/src/com/mygdx/game/system/UmbrellaSystem.java
@@ -5,6 +5,7 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Bezier;
 import com.badlogic.gdx.math.CatmullRomSpline;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
@@ -30,151 +31,83 @@ public class UmbrellaSystem extends IteratingSystem {
 
     public static final float UMBRELLA_SCALE = 0.4f;
 
-    private int randVelocityXmin = 110;
-    private int randVelocityXmax = 150;
-    private int randVelocityYmin = 45;
-    private int randVelocityYmax = 47;
-    
     public Random random = new Random();
     private ComponentMapper<UmbrellaComponent> mapper = ComponentMapper.getFor(UmbrellaComponent.class);
     private ComponentMapper<FlowerPublicComponent> fccMapper = ComponentMapper.getFor(FlowerPublicComponent.class);
 
-
-
     public UmbrellaSystem() {
         super(Family.all(UmbrellaComponent.class).get());
-
-
     }
 
     @Override
     protected void processEntity(Entity entity, float deltaTime) {
         SpriteAnimationStateComponent sasc = ComponentRetriever.get(entity, SpriteAnimationStateComponent.class);
-        UmbrellaComponent uc = mapper.get(entity);
-        DimensionsComponent dc = ComponentRetriever.get(entity, DimensionsComponent.class);
+        if (!GameScreenScript.isPause && !GameScreenScript.isGameOver) {
+
+            UmbrellaComponent uc = mapper.get(entity);
+            DimensionsComponent dc = ComponentRetriever.get(entity, DimensionsComponent.class);
             TransformComponent tc = ComponentRetriever.get(entity, TransformComponent.class);
             tc.scaleX = UMBRELLA_SCALE;
             tc.scaleY = UMBRELLA_SCALE;
 
+            uc.current += Gdx.graphics.getDeltaTime() * uc.speed;
 
-        uc.current += Gdx.graphics.getDeltaTime() * uc.speed;
-        if(uc.current >= 1)
-            uc.current -= 1;
-        uc.myCatmull.valueAt(uc.out, uc.current);
-        tc.x = uc.out.x;
-        tc.y = uc.out.y;
+            if (uc.state == UmbrellaComponent.State.PUSH) {
+                uc.dataSet = new Vector2[3];
+                uc.dataSet[0] = new Vector2(tc.x, tc.y);
+                uc.dataSet[1] = new Vector2(-500, Gdx.graphics.getHeight() / 2);
+                uc.dataSet[2] = new Vector2(Gdx.graphics.getWidth() - 30, Gdx.graphics.getHeight() / 2);
 
+                uc.myCatmull = new Bezier<Vector2>(uc.dataSet);
+                uc.out = new Vector2(340, Gdx.graphics.getHeight() / 4);
+                uc.myCatmull.valueAt(uc.out, 5);
+                uc.myCatmull.derivativeAt(uc.out, 5);
+
+                uc.state = UmbrellaComponent.State.FLY;
+            }
+
+            if (uc.current >= 1 && uc.state == UmbrellaComponent.State.FLY) {
+                uc.dataSet[0] = new Vector2(uc.dataSet[2].x, uc.dataSet[2].y);
+                uc.dataSet[2] = new Vector2(Gdx.graphics.getWidth() - 30, random.nextInt(((Gdx.graphics.getHeight() - 100) - 100)) + 100);
+                uc.dataSet[1] = new Vector2(-1100, (uc.dataSet[2].y + uc.dataSet[0].y) / 2);
+                System.out.println("0: " + uc.dataSet[0].y + " || 1: " + uc.dataSet[1].y + " || 2: " + uc.dataSet[2].y);
+
+                uc.myCatmull = new Bezier<Vector2>(uc.dataSet);
+                uc.out = new Vector2(340, Gdx.graphics.getHeight() / 4);
+                uc.myCatmull.valueAt(uc.out, 5);
+                uc.myCatmull.derivativeAt(uc.out, 5);
+
+                uc.current -= 1;
+            }
+
+            uc.myCatmull.valueAt(uc.out, uc.current);
+            tc.x = uc.out.x;
+            tc.y = uc.out.y;
+
+            sasc.paused = false;
 //
-//        if (!GameScreenScript.isPause && !GameScreenScript.isGameOver) {
-//            sasc.paused = false;
+            FlowerPublicComponent fcc = fccMapper.get(entity);
 //
-//            DimensionsComponent dc = ComponentRetriever.get(entity, DimensionsComponent.class);
-//            TransformComponent tc = ComponentRetriever.get(entity, TransformComponent.class);
-//            tc.scaleX = UMBRELLA_SCALE;
-//            tc.scaleY = UMBRELLA_SCALE;
-//
-//            FlowerPublicComponent fcc = fccMapper.get(entity);
-//            UmbrellaComponent uc = mapper.get(entity);
-//
-//            move(deltaTime, tc, uc, entity);
-//            updateRect(uc, tc, dc);
-//
-//            if (checkCollision(uc, fcc)) {
-//                fcc.isCollision = true;
-//                GameStage.sceneLoader.getEngine().removeEntity(entity);
-//
-//                //temp
-//                fcc.totalScore -= fcc.score;
-//                fcc.score *= uc.pointsMult;
-//                fcc.totalScore += fcc.score;
-//
-//                reloadScoreLabel(fcc);
-//            }
-//        } else {
-//            sasc.paused = true;
-//        }
+            updateRect(uc, tc, dc);
+
+            if (checkCollision(uc, fcc)) {
+                fcc.isCollision = true;
+                GameStage.sceneLoader.getEngine().removeEntity(entity);
+
+                //temp
+                fcc.totalScore -= fcc.score;
+                fcc.score *= uc.pointsMult;
+                fcc.totalScore += fcc.score;
+
+                reloadScoreLabel(fcc);
+            }
+        } else {
+            sasc.paused = true;
+        }
     }
 
     private void reloadScoreLabel(FlowerPublicComponent fcc) {
-        scoreLabelComponent.text.replace(0, scoreLabelComponent.text.capacity(), "" + fcc.score  + "/" + fcc.totalScore);
-    }
-
-    private void move(float deltaTime, TransformComponent tc, UmbrellaComponent uc, Entity entity) {
-//        if (uc.state == UmbrellaComponent.State.PUSH) {
-//            pushUmbrella(uc, tc);
-//            uc.state = UmbrellaComponent.State.FLY;
-//        } else {
-//            flyUmbrella(uc, tc, deltaTime);
-//        }
-//        if (isOutOfBounds(uc)) {
-//            pushUmbrella(uc, tc);
-//        }
-
-//        TintComponent tic = entity.getComponent(TintComponent.class);
-//        TransformComponent tc = entity.getComponent(TransformComponent.class);
-//        DimensionsComponent dc = entity.getComponent(DimensionsComponent.class);
-        FlowerPublicComponent fcc = fccMapper.get(entity);
-//        BugJuiceBubbleComponent uc = mapper.get(entity);
-        if (!uc.began) {
-            begin(uc, tc);
-            uc.began = true;
-        }
-        uc.time += deltaTime;
-        uc.complete = uc.time >= uc.duration;
-        float percent;
-        if (uc.complete) {
-            percent = 1;
-        } else {
-            percent = uc.time / uc.duration;
-            if (uc.interpolation != null) percent = uc.interpolation.apply(percent);
-        }
-        update(uc, tc, uc.reverse ? 1 - percent : percent);
-        if (uc.complete) end(fcc, entity);
-    }
-
-    public void update(UmbrellaComponent uc, TransformComponent tc, float percent) {
-        setPosition(tc, uc.startX + (uc.endX - uc.startX) * percent * percent, uc.startY + (uc.endY - uc.startY) * percent);
-//        if (tc.x >= Gdx.graphics.getWidth()-10){
-//            uc.startX = tc.x;
-//            uc.startY = tc.y;
-//            uc.endY = random.nextInt(Gdx.graphics.getHeight());
-//            uc.endX = random.nextInt(Gdx.graphics.getWidth());
-//
-//        }
-    }
-
-    protected void begin(UmbrellaComponent uc, TransformComponent tc) {
-        uc.startX = tc.x;
-        uc.startY = tc.y;
-    }
-
-    protected void end(FlowerPublicComponent fcc, Entity entity) {
-//        scoreLabelComponent.text.replace(0, scoreLabelComponent.text.capacity(), "" + fcc.score + "/" + fcc.totalScore);
-
-        GameStage.sceneLoader.getEngine().removeEntity(entity);
-    }
-
-    public void setPosition(TransformComponent tc, float x, float y) {
-        tc.x = x;
-        tc.y = y;
-    }
-
-    public void pushUmbrella(UmbrellaComponent uc, TransformComponent tc) {
-        uc.velocityX = ((random.nextInt(randVelocityXmax - randVelocityXmin) + randVelocityXmin) * -1) * uc.speedIncrCoeficient;
-        if (tc.y > Gdx.graphics.getHeight() / 2) {
-            uc.velocityY = (random.nextInt((randVelocityYmax - randVelocityYmin) + randVelocityYmin) * -1) * uc.speedIncrCoeficient;
-        } else {
-            uc.velocityY = (random.nextInt((randVelocityYmax - randVelocityYmin) + randVelocityYmin)) * uc.speedIncrCoeficient;
-        }
-        uc.gravity = Math.abs(uc.velocityX / (7 - uc.speedIncrCoeficient * uc.gravityDecreaseMultiplier));
-        uc.speedIncrCoeficient += 0.1f;
-        uc.gravityDecreaseMultiplier -= 0.05f;
-    }
-
-    public void flyUmbrella(UmbrellaComponent uc, TransformComponent tc, float delta) {
-        uc.velocityX += uc.gravity * delta;
-        tc.x += uc.velocityX * delta;
-        tc.y += uc.velocityY * delta;
+        scoreLabelComponent.text.replace(0, scoreLabelComponent.text.capacity(), "" + fcc.score + "/" + fcc.totalScore);
     }
 
     public void updateRect(UmbrellaComponent uc, TransformComponent tc, DimensionsComponent dc) {
@@ -182,10 +115,6 @@ public class UmbrellaSystem extends IteratingSystem {
         uc.boundsRect.y = (int) tc.y;
         uc.boundsRect.width = (int) dc.width * tc.scaleX;
         uc.boundsRect.height = (int) dc.height * tc.scaleY;
-    }
-
-    public boolean isOutOfBounds(UmbrellaComponent uc) {
-        return uc.boundsRect.getX() >= Gdx.graphics.getWidth();
     }
 
     private boolean checkCollision(UmbrellaComponent bc, FlowerPublicComponent fcc) {
