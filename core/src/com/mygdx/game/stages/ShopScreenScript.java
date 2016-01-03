872d6cc12c501a1fad673296aa0cf8f27db01bd2
Nastyshka <Nastya.rudyk@gmail.com>
2016-01-03 15:26:10 +0200

    * Shop tapping implemented, started icon image change

--- core/src/com/mygdx/game/stages/ShopScreenScript.java
+++ core/src/com/mygdx/game/stages/ShopScreenScript.java
@@ -3,12 +3,16 @@ package com.mygdx.game.stages;
 import com.badlogic.ashley.core.Entity;
 import com.mygdx.game.entity.componets.VanityComponent;
 import com.mygdx.game.utils.SaveMngr;
+import com.uwsoft.editor.renderer.components.DimensionsComponent;
 import com.uwsoft.editor.renderer.components.LayerMapComponent;
+import com.uwsoft.editor.renderer.components.TransformComponent;
 import com.uwsoft.editor.renderer.components.additional.ButtonComponent;
+import com.uwsoft.editor.renderer.data.CompositeItemVO;
 import com.uwsoft.editor.renderer.scripts.IScript;
 import com.uwsoft.editor.renderer.utils.ComponentRetriever;
 import com.uwsoft.editor.renderer.utils.ItemWrapper;
 
+import javax.xml.transform.TransformerFactory;
 import java.util.List;
 
 /**
@@ -20,7 +24,6 @@ public class ShopScreenScript implements IScript {
     private ItemWrapper shopItem;
 //    private int spawnCounter = 0;
 
-
     public ShopScreenScript(GameStage stage) {
         this.stage = stage;
     }
@@ -29,35 +32,53 @@ public class ShopScreenScript implements IScript {
     public void init(Entity item) {
         shopItem = new ItemWrapper(item);
 
-//        getAllVanities2();
         stage.sceneLoader.addComponentsByTagName("button", ButtonComponent.class);
-        getAllVanities();
+
+        getAllAllVanities();
 
         addBackButtonPlease();
+
     }
 
-    private void getAllVanities() {
-//        Entity backBtn = shopItem.getChild("btn_shop_item_1").getEntity();
+    private void getAllAllVanities(){
         List<VanityComponent> vanityComponentList = SaveMngr.getAllVanity();
+        int x = 173;
+        int y = 289;
 
         for (final VanityComponent vc : vanityComponentList) {
 
-            Entity btn = shopItem.getChild(vc.icon).getEntity();
 
-            final LayerMapComponent lc = ComponentRetriever.get(btn, LayerMapComponent.class);
-            btn.getComponent(ButtonComponent.class).addListener(new ButtonComponent.ButtonListener() {
+            CompositeItemVO tempC = GameStage.sceneLoader.loadVoFromLibrary("btn_shop_icon_lib").clone();
+            final Entity bagEntity = GameStage.sceneLoader.entityFactory.createEntity(GameStage.sceneLoader.getRoot(), tempC);
+            GameStage.sceneLoader.entityFactory.initAllChildren(GameStage.sceneLoader.getEngine(), bagEntity, tempC.composite);
+            GameStage.sceneLoader.getEngine().addEntity(bagEntity);
+
+            CompositeItemVO tempItemC = GameStage.sceneLoader.loadVoFromLibrary("item_white_beard_n").clone();
+            final Entity beardE = GameStage.sceneLoader.entityFactory.createEntity(GameStage.sceneLoader.getRoot(), tempItemC);
+            GameStage.sceneLoader.entityFactory.initAllChildren(GameStage.sceneLoader.getEngine(), beardE, tempItemC.composite);
+            GameStage.sceneLoader.getEngine().addEntity(beardE);
+
+            TransformComponent tc = bagEntity.getComponent(TransformComponent.class);
+            tc.x = x;
+            tc.y = y;
+
+            beardE.add(new ButtonComponent());
+            shopItem.getChild("btn_shop_icon_lib").addChild(beardE);
+            TransformComponent tcb = beardE.getComponent(TransformComponent.class);
+            tcb.x = tc.x;
+            tcb.y = tc.y;
+
+
+            bagEntity.add(new ButtonComponent());
+            bagEntity.getComponent(ButtonComponent.class).addListener(new ButtonComponent.ButtonListener() {
                 @Override
                 public void touchUp() {
-                    lc.getLayer("normal").isVisible = true;
-                    lc.getLayer("Default").isVisible = false;
-                    lc.getLayer("pressed").isVisible = false;
+
                 }
 
                 @Override
                 public void touchDown() {
-                    lc.getLayer("normal").isVisible = false;
-                    lc.getLayer("Default").isVisible = false;
-                    lc.getLayer("pressed").isVisible = true;
+
                 }
 
                 @Override
@@ -67,6 +88,7 @@ public class ShopScreenScript implements IScript {
 
                 }
             });
+            x += 250;
         }
     }
 
