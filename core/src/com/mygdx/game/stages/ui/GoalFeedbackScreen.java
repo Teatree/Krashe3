702dd78064fde0dae678018c6c2a815e41aa90ae
Nastyshka <Nastya.rudyk@gmail.com>
2016-04-09 14:46:24 +0300

    goal feedback is better now

--- core/src/com/mygdx/game/stages/ui/GoalFeedbackScreen.java
+++ core/src/com/mygdx/game/stages/ui/GoalFeedbackScreen.java
@@ -2,8 +2,10 @@ package com.mygdx.game.stages.ui;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Interpolation;
 import com.mygdx.game.entity.componets.Goal;
 import com.mygdx.game.stages.GameStage;
+import com.mygdx.game.utils.EffectUtils;
 import com.mygdx.game.utils.GlobalConstants;
 import com.uwsoft.editor.renderer.components.*;
 import com.uwsoft.editor.renderer.components.label.LabelComponent;
@@ -32,6 +34,8 @@ public class GoalFeedbackScreen {
     public static final float GOAL_SCALE = 0.75f;
     public static final int DELAY_ON_ANIMATION = 5000;
     public static final int GOAL_INIT_POS_Y = 556;
+    public static final float INITIAL_DELAY = 1.2f;
+    public static final float MOVE_TILES_DELAY = 0.3f;
     public static boolean shouldShow;
 
     private List<Entity> tiles;
@@ -59,6 +63,7 @@ public class GoalFeedbackScreen {
 
     public void init(boolean isNewLevel) {
 
+        aniPlayingIndex = -1;
         this.isNewLevel = isNewLevel;
         if (!isNewLevel) {
 
@@ -75,12 +80,12 @@ public class GoalFeedbackScreen {
                 prevLvlTiles.add(tile);
             }
 
-            if (tiles != null) {
-                for (Entity tile : tiles) {
-                    tile.getComponent(TransformComponent.class).x = GlobalConstants.FAR_FAR_AWAY_X;
-                    sceneLoader.getEngine().removeEntity(tile);
-                }
-            }
+//            if (tiles != null) {
+//                for (Entity tile : tiles) {
+//                    tile.getComponent(TransformComponent.class).x = GlobalConstants.FAR_FAR_AWAY_X;
+//                    sceneLoader.getEngine().removeEntity(tile);
+//                }
+//            }
         }
 
         tiles = new ArrayList<>();
@@ -118,33 +123,53 @@ public class GoalFeedbackScreen {
         feedbackEntity.getComponent(TransformComponent.class).y = POS_Y;
         feedbackEntity.getComponent(ZIndexComponent.class).setZIndex(190);
 
-        final Entity goalLabel = gameItem.getChild(GOALFEEDBACK).getChild(LBL_DIALOG).getEntity();
-        LabelComponent goalsLabelComp = goalLabel.getComponent(LabelComponent.class);
-        goalsLabelComp.text.replace(0, goalsLabelComp.text.capacity(), " \n     " + GameStage.gameScript.fpc.level.name + " \n ");
+//        final Entity goalLabel = gameItem.getChild(GOALFEEDBACK).getChild(LBL_DIALOG).getEntity();
+//        LabelComponent goalsLabelComp = goalLabel.getComponent(LabelComponent.class);
+//        goalsLabelComp.text.replace(0, goalsLabelComp.text.capacity(), " \n     " + GameStage.gameScript.fpc.level.name + " \n ");
 
         if (prevLvlTiles != null || !prevLvlTiles.isEmpty() || !isPause) {
-            for(SpriterComponent s: tilesScs)
-            {
+            for (SpriterComponent s : tilesScs) {
                 tilesScs2.add(s);
+
 //                s.player.speed = 25;
 //                s.player.setAnimation(0);
             }
 //            tilesScs = new ArrayList<>();
             int y = GOAL_INIT_POS_Y;
+            int i = 1;
+
+            float delay = 0;
             for (Entity e : prevLvlTiles) {
                 e.getComponent(TransformComponent.class).x = GOAL_INIT_POS_X;
                 e.getComponent(TransformComponent.class).y = y;
                 e.getComponent(TintComponent.class).color.a = 1;
 
-                if (e.getComponent(NodeComponent.class) != null && e.getComponent(NodeComponent.class).children != null && e.getComponent(NodeComponent.class).children.size != 0) {
+                ActionComponent ac = new ActionComponent();
+                Actions.checkInit();
+
+                if (i == 1) {
+                    delay = INITIAL_DELAY;
+                } else {
+                    delay += MOVE_TILES_DELAY;
+                }
+                i++;
+
+                ac.dataArray.add(Actions.sequence(Actions.delay(delay),
+                        Actions.moveTo(-e.getComponent(DimensionsComponent.class).width,
+                                e.getComponent(TransformComponent.class).y, 1.5f, Interpolation.exp10In)));
+                e.add(ac);
+
+                if (e.getComponent(NodeComponent.class) != null && e.getComponent(NodeComponent.class).children != null
+                        && e.getComponent(NodeComponent.class).children.size != 0) {
                     for (Entity e2 : e.getComponent(NodeComponent.class).children) {
                         TintComponent tc = e2.getComponent(TintComponent.class);
                         tc.color.a = 1;
-                        e2.getComponent(TransformComponent.class).x = GOAL_INIT_POS_X;
+
                         e2.getComponent(TintComponent.class).color.a = 1;
-                        e2.getComponent(ZIndexComponent.class).setZIndex(2000);
+//                        e2.getComponent(ZIndexComponent.class).setZIndex(2000);
                     }
                 }
+                e.getComponent(TransformComponent.class).x = GOAL_INIT_POS_X;
                 e.getComponent(ZIndexComponent.class).setZIndex(2000);
                 y -= GOAL_STEP_Y;
             }
@@ -152,8 +177,28 @@ public class GoalFeedbackScreen {
         if (tiles == null || tiles.isEmpty() || !isPause) {
             int y = GOAL_INIT_POS_Y;
             tilesScs = new ArrayList<>();
+
+            int i = 1;
+            float delay = INITIAL_DELAY + prevLvlTiles.size() * MOVE_TILES_DELAY + 2;
             for (Goal g : GameStage.gameScript.fpc.level.getGoals()) {
-                tiles.add(createGoalTile(g, y));
+                Entity newTile = createGoalTile(g, y);
+
+                ActionComponent ac = new ActionComponent();
+                Actions.checkInit();
+
+                if (i == 1) {
+                    delay = INITIAL_DELAY + prevLvlTiles.size() * MOVE_TILES_DELAY + 2;
+                } else {
+                    delay += 0.1f;
+                }
+                i++;
+
+                ac.dataArray.add(Actions.sequence(Actions.delay(delay),
+                        Actions.moveTo(GOAL_INIT_POS_X, newTile.getComponent(TransformComponent.class).y, 1.5f, Interpolation.exp10Out)));
+                newTile.add(ac);
+
+                tiles.add(newTile);
+
                 y -= GOAL_STEP_Y;
             }
         }
@@ -163,7 +208,6 @@ public class GoalFeedbackScreen {
     }
 
     private Entity createGoalTile(Goal goal, int y) {
-
         CompositeItemVO tempC;
         tempC = sceneLoader.loadVoFromLibrary(ACHIEVED_GOAL_LIB).clone();
 
@@ -172,7 +216,7 @@ public class GoalFeedbackScreen {
         sceneLoader.getEngine().addEntity(tile);
 
         if (isNewLevel) {
-            tile.getComponent(TransformComponent.class).x = Gdx.graphics.getWidth() - 390;
+            tile.getComponent(TransformComponent.class).x = Gdx.graphics.getWidth();
         } else {
             tile.getComponent(TransformComponent.class).x = POS_X + GOAL_INIT_POS_X;
         }
@@ -213,41 +257,55 @@ public class GoalFeedbackScreen {
     }
 
     public void update() {
-            fade(feedbackEntity, isGoalFeedbackOpen);
-            int i = 0;
-            while (i < tiles.size()) {
-                fade(tiles.get(i), isGoalFeedbackOpen);
-                if (GameStage.gameScript.fpc.level.getGoals().get(i).justAchieved && !isAniPlaying) {
-                    tilesScs.get(i).player.speed = 4;
-                    isAniPlaying = true;
-                    aniPlayingIndex = i;
-                    GameStage.gameScript.fpc.level.getGoals().get(i).justAchieved = false;
-                } else if (tilesScs.get(i).player.getTime() >=
-                        tilesScs.get(i).player.getAnimation().length - 20) {
-                    tilesScs.get(i).player.speed = 0;
-                    tilesScs.get(i).player.setTime(tilesScs.get(i).player.getAnimation().length - 20);
-                }
-                i++;
+        fade(feedbackEntity, isGoalFeedbackOpen);
+        int i = 0;
+        while (i < tiles.size()) {
+            fade(tiles.get(i), isGoalFeedbackOpen);
+            if (GameStage.gameScript.fpc.level.getGoals().get(i).justAchieved && !isAniPlaying) {
+                tilesScs.get(i).player.speed = 4;
+                isAniPlaying = true;
+                aniPlayingIndex = i;
+                GameStage.gameScript.fpc.level.getGoals().get(i).justAchieved = false;
+            } else if (tilesScs.get(i).player.getTime() >=
+                    tilesScs.get(i).player.getAnimation().length - 20) {
+                tilesScs.get(i).player.speed = 0;
+                tilesScs.get(i).player.setTime(tilesScs.get(i).player.getAnimation().length - 20);
             }
-            if (!tilesScs.isEmpty() && tilesScs.get(aniPlayingIndex).player.getTime() >=
-                    tilesScs.get(aniPlayingIndex).player.getAnimation().length - 20) {
-                isAniPlaying = false;
+            i++;
+        }
+
+        if (!tilesScs.isEmpty() && aniPlayingIndex >= 0 && tilesScs.get(aniPlayingIndex).player.getTime() >=
+                tilesScs.get(aniPlayingIndex).player.getAnimation().length - 20) {
+            isAniPlaying = false;
+        }
+
+        if (prevLvlTiles != null && prevLvlTiles.get(prevLvlTiles.size()-1).getComponent(TransformComponent.class).x <=
+                -prevLvlTiles.get(prevLvlTiles.size()-1).getComponent(DimensionsComponent.class).width ){
+            final Entity goalLabel = gameItem.getChild(GOALFEEDBACK).getChild(LBL_DIALOG).getEntity();
+
+            LabelComponent goalsLabelComp = goalLabel.getComponent(LabelComponent.class);
+            if(!goalsLabelComp.text.toString().equals(GameStage.gameScript.fpc.level.name)) {
+                EffectUtils.playYellowStarsParticleEffect(goalLabel.getComponent(TransformComponent.class).x,
+                        goalLabel.getComponent(TransformComponent.class).y);
+                goalsLabelComp.text.replace(0, goalsLabelComp.text.capacity(), GameStage.gameScript.fpc.level.name);
             }
+        }
 
-            if (Gdx.input.justTouched() && isGoalFeedbackOpen) {
-                if (GameStage.gameScript.fpc.level.checkAllGoals()) {
-                    gameScript.giftScreen.show();
 
-                    isGoalFeedbackOpen = false;
+        if (Gdx.input.justTouched() && isGoalFeedbackOpen) {
+            if (GameStage.gameScript.fpc.level.checkAllGoals()) {
+                gameScript.giftScreen.show();
+
+                isGoalFeedbackOpen = false;
+            } else {
+                if (GameStage.gameScript.giftScreen.isGiftScreenOpen) {
+                    GameStage.gameScript.giftScreen.isGiftScreenOpen = false;
+                    GameStage.gameScript.giftScreen.hide();
                 } else {
-                    if (GameStage.gameScript.giftScreen.isGiftScreenOpen) {
-                        GameStage.gameScript.giftScreen.isGiftScreenOpen = false;
-                        GameStage.gameScript.giftScreen.hide();
-                    } else {
-                        GameStage.gameScript.stage.initResult();
-                        isGoalFeedbackOpen = false;
-                    }
+                    GameStage.gameScript.stage.initResult();
+                    isGoalFeedbackOpen = false;
                 }
             }
         }
+    }
 }
