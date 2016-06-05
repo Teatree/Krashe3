9067eecf58726a038cc0517278131ab29997830f
Teatree <4aynoedepebo@gmail.com>
2016-06-05 15:30:05 +0300

    * fly animation implemented

--- core/src/com/mygdx/game/system/BugSpawnSystem.java
+++ core/src/com/mygdx/game/system/BugSpawnSystem.java
@@ -21,11 +21,11 @@ public class BugSpawnSystem extends EntitySystem {
 
     // spawn probability
     // combined has to be equal to 100
-    public static final int DRUNK_SPAWN_PROB = 10;
-    public static final int SIMPLE_SPAWN_PROB = 50;
-    public static final int CHARGER_SPAWN_PROB = 7;
-    public static final int QUEENBEE_SPAWN_PROB = 30;
-    public static final int BEE_SPAWN_PROB = 3;
+    public static final int DRUNK_SPAWN_PROB = 70;
+    public static final int SIMPLE_SPAWN_PROB = 1;
+    public static final int CHARGER_SPAWN_PROB = 1;
+    public static final int QUEENBEE_SPAWN_PROB = 10;
+    public static final int BEE_SPAWN_PROB = 18;
 
     public static int curDrunkProb = DRUNK_SPAWN_PROB;
     public static int curSimpleProb = SIMPLE_SPAWN_PROB;
