98e844e700898a230ab7d21584ed8f5c43f030b9
Teatree <4aynoedepebo@gmail.com>
2016-08-26 22:54:07 +0300

    * Fixed fonts + added assets for AP upgrade indicators

--- core/src/com/mygdx/etf/stages/ResultScreenScript.java
+++ core/src/com/mygdx/etf/stages/ResultScreenScript.java
@@ -24,7 +24,7 @@ import static com.mygdx.etf.stages.ui.PromoWindow.*;
 
 public class ResultScreenScript implements IScript {
 
-    public static final int MAX_PROGRESS_BAR_WIDTH = 690;
+    public static final int MAX_PROGRESS_BAR_WIDTH = 670;
     public static final double PERCENTS_TO_OFFER_AD = 0.99;
     public static final int PROGRESS_BAR_STEP = 12;
     public static final String IMG_PROGRESS_BAR = "img_progress_bar";
